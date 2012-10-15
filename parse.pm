60e99b7f7f2c8534f1ad3510d21d18e7b53cbe05
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2012-10-15 19:30:39 -0400

    Correct output, added skater analysis
    This isn't tested 100%, but it's producing the correct score
    calculations for at least one school (only tested specifically for one
    school). This now outputs information on every entrant about their
    total points and point generation ability. I'd like to compute
    something about dance / freestyle / level stratification to get a
    better idea of how teams are structured and where teams need
    improvement.

--- parse.pm
+++ parse.pm
@@ -1,167 +1,242 @@
 #!/usr/bin/perl
 
 # Do withdrawls affect the number of points that are awarded?
+# 
+# The total number of entrants per event is not being counted if the
+# school didn't get points.
 
 #$filename = "corpus/2012_mit_results.txt";
 $filename = "corpus/2012_cornell_results.txt";
+$results_dir = "./results";
 
-open FILE, "<", $filename or die "Unable to open $filename";
+chdir $results_dir or die "Directory",$results_dir,"not found!";
+@files = sort(<*>);
+chdir $files[scalar(@files)-1] or die "Directory",$files[scalar(@files)-1],"not found";
+@files = sort(<*>);
+foreach (@files){
+    if ($_ =~ m/\.results$/){
+        print "  Processing ",$_,"\n";
+        
+        $file_out = $_;
+        $file_out =~ s/\..+$/\.out/;
 
-@lines = <FILE>;
+        open FILE_RESULTS, "<", $_ or die "Unable to open $_";
+        open FILE_OUT, ">", $file_out or die "Unable to open $_";
 
-foreach (@lines){
-    # Ignore line
-    if ($_ =~ m/###/){
-    }
-    # Line is a group
-    elsif ($_ =~ m/^[a-zA-Z]/){
-        chomp($_);
-        $event_name = $_;
-    }
-    # Line is a skater
-    else{
-        push @{$event_hash{$event_name}}, $_;
-    }
-}
+        @lines = <FILE_RESULTS>;
 
-foreach $event (sort keys %event_hash){
-    $num_skaters = 0;
-    # check for championship event
-    if ($event =~ m/Championship/){
-        $championship = 1;
-    }
-    else{
-        $championship = 0;
-    }
-    # compute number of skaters
-    foreach (@{$event_hash{$event}}){
-# If the number of points awarded decreases as a result of a
-# withdrawl, the commented out section below should be used.
-        # if ($_ !~ m/^  W,/){
-        #     $num_skaters++;
-        # }
-        $num_skaters++;
-    }
-    # compute starting point value
-    if ($num_skaters >= 5){
-        $points = 5 + $championship * 2;
-    }
-    elsif ($num_skaters >= 4){
-        $points = 4 + $championship * 2;
-    }
-    elsif ($num_skaters >= 2){
-        $points = 3 + $championship * 2;
-    }
-    else {
-        $points = 1 + $championship * 2;
-    }
-    # compute tie array
-    $old_placement = 0;
-    $skater_count = 0;
-    $tie_count = 1;
-    foreach (@{$event_hash{$event}}){
-        if ($_ =~ m/($old_placement)/){
-            $tie_count++;
-        }
-        else {
-            $old_placement++;
-            if ($skater_count > 0){
-                $points_total = 0;
-                for ($i = $skater_count - $tie_count; $i < $skater_count; $i++){
-                    chomp(@{$event_hash{$event}}[$i]);
-                    $points_total += $points;
-                    if ($points > 0 + $championship * 3){
-                        $points --;
-                    }
-                    else {
-                        $points = 0;
-                    }
-                }
-                $points_total /= $tie_count;
-                for ($i = $skater_count - $tie_count; $i < $skater_count; $i++){
-                    @{$event_hash{$event}}[$i] .= ",$points_total";
-                }
-                $tie_count = 1;
+        foreach (@lines){
+            $_ =~ s///;
+            # Ignore line
+            if ($_ =~ m/[#]+/){
+            }
+            # Line is a group
+            elsif ($_ =~ m/^[a-zA-Z]/){
+                chomp($_);
+                $event_name = $_;
+            }
+            # Line is a skater
+            else{
+                push @{$event_hash{$event_name}}, $_;
             }
         }
-        $skater_count++;
-    }
-    # last place skater handling
-    $points_total = 0;
-    for ($i = $skater_count - $tie_count; $i < $skater_count; $i++){
-        chomp(@{$event_hash{$event}}[$i]);
-        $points_total += $points;
-        if ($points > 0 + $championship * 3){
-            $points --;
+
+        foreach $event (sort keys %event_hash){
+            $num_skaters = 0;
+            $withdrawl_skaters = 0;
+            # check for championship event
+            if ($event =~ m/Championship/ | $event =~ m/International/){
+                $championship = 1;
+            }
+            else{
+                $championship = 0;
+            }
+            # compute number of skaters
+            foreach (@{$event_hash{$event}}){
+# If the number of points awarded decreases as a result of a
+# withdrawl, the commented out section below should be used.
+                if ($_ !~ m/^[ ]+W,/){
+                    $num_skaters++;
+                }
+                elsif ($num_skaters<5){
+                    $withdrawl_skaters++;
+                    $num_skaters++;
+                }
+#                $num_skaters++;
+            }
+            # compute starting point value
+            if ($num_skaters >= 5){
+                $points = 5 + $championship * 2;
+            }
+            elsif ($num_skaters >= 4){
+                $points = 4 + $championship * 2;
+            }
+            elsif ($num_skaters >= 2){
+                $points = 3 + $championship * 2;
+            }
+            else {
+                $points = 1 + $championship * 2;
+            }
+            # compute tie array
+            $old_placement = 0;
+            $skater_count = 0;
+            $tie_count = 1;
+            foreach (@{$event_hash{$event}}){
+                if ($_ =~ m/($old_placement)/){
+                    $tie_count++;
+                }
+                else {
+                    $old_placement++;
+                    if ($skater_count > 0){ # if this is not the first skater
+                        $points_total = 0;
+                        for ($i = $skater_count - $tie_count; $i < $skater_count; $i++){
+                            chomp(@{$event_hash{$event}}[$i]);
+                            $points_total += $points;
+                            if (($points > 0 + $championship * 3) & 
+                                !()){
+                                $points --;
+                            }
+                            else {
+                                $points = 0;
+                            }
+                        }
+                        $points_total /= $tie_count;
+                        for ($i = $skater_count - $tie_count; $i < $skater_count; $i++){
+                            @{$event_hash{$event}}[$i] .= ",$points_total";
+                            $school = @{$event_hash{$event}}[$i];
+                            chomp($school);
+                            $school =~ s/^[ ]+[0-9W]+,[a-zA-Z'\- ]+,//;
+                            $school =~ s/,[0-9]+$//;
+                            $skater = @{$event_hash{$event}}[$i];
+                            chomp($skater);
+                            $skater =~ s/^[ ]+[0-9W]+,//;
+                            $skater =~ s/,.+$//;
+                            $skater_point_hash{$skater} += $points_total;
+                            $skater_start_hash{$skater} += 1;
+                            $skater_school_hash{$skater} = $school;
+                        }
+                        $tie_count = 1;
+                    }
+                }
+                $skater_count++;
+            }
+            # last place skater handling
+            $points_total = 0;
+            for ($i = $skater_count - $tie_count; $i < $skater_count; $i++){
+                chomp(@{$event_hash{$event}}[$i]);
+                $points_total += $points;
+                if ($points > 0 + $championship * 3){
+                    $points --;
+                }
+                else {
+                    $points = 0;
+                }
+            }
+            $points_total /= $tie_count;
+            for ($i = $skater_count - $tie_count; $i < $skater_count; $i++){
+                $skater = @{$event_hash{$event}}[$i];
+                chomp($skater);
+                $skater =~ s/^[ ]+[0-9W]+,//;
+                $skater =~ s/,.+$//;
+                $school = @{$event_hash{$event}}[$i];
+                chomp($school);
+                $school =~ s/^.+,//;
+                if (@{$event_hash{$event}}[$i] =~ m/[ ]+W/){
+                    @{$event_hash{$event}}[$i] .= ",0";
+                }
+                else{
+                    @{$event_hash{$event}}[$i] .= ",$points_total";
+                    $skater_point_hash{$skater} += $points_total;
+                }
+                $skater_start_hash{$skater} += 1;
+                $skater_school_hash{$skater} = $school;
+            }
         }
-        else {
-            $points = 0;
+
+        print FILE_OUT "---------------------------------------- Event Results\n";
+        foreach $event_name (sort keys %event_hash){
+            print FILE_OUT $event_name."\n";
+            foreach (@{$event_hash{$event_name}}){
+                print FILE_OUT $_."\n";
+            }
         }
-    }
-    $points_total /= $tie_count;
-    for ($i = $skater_count - $tie_count; $i < $skater_count; $i++){
-        @{$event_hash{$event}}[$i] .= ",$points_total";
-    }
-}
 
-foreach $event_name (sort keys %event_hash){
-    print $event_name."\n";
-    foreach (@{$event_hash{$event_name}}){
-        print $_."\n";
-    }
-}
+        foreach $event_name (sort keys %event_hash){
+            foreach (@{$event_hash{$event_name}}){
+                $school = $_;
+                $school =~ s/^[ ]+[0-9W]+,[a-zA-Z-'. ]+,//;
+                $school =~ s/,$//;
+                $points = $school;
+                $school =~ s/,.+//;
+                $points =~ s/^.+,//;
+                $total_hash{$school} += $points;
+                $total_starts_hash{$school} += 1;
+                $event_hash_total{$event_name}{$school} += $points;
+                $starts_hash{$event_name}{$school} += 1;
+            }
+        }
 
-foreach $event_name (sort keys %event_hash){
-    foreach (@{$event_hash{$event_name}}){
-        $school = $_;
-        $school =~ s/^  [0-9W]+,[a-zA-Z-'. ]+,//;
-        $points = $school;
-        $school =~ s/,.+//;
-        $points =~ s/^.+,//;
-        $total_hash{$school} += $points;
-        $event_hash_total{$event_name}{$school} += $points;
-        $starts_hash{$event_name}{$school} += 1;
-    }
-}
+        print FILE_OUT "---------------------------------------- Skater Results\n";
+        foreach $school (sort {$skater_school_hash{$a} <=> $skater_school_hash{$b}} keys %skater_school_hash){
+            foreach $skater (sort {$skater_point_hash{$b} <=> $skater_point_hash{$a}} keys %skater_point_hash){
+                if ($skater_school_hash{$skater} eq $school) {
+                    print FILE_OUT "  ",$skater,",",$skater_school_hash{$skater},",",$skater_point_hash{$skater},",",$skater_start_hash{$skater},",",sprintf("%0.2f",$skater_point_hash{$skater}/$skater_start_hash{$skater}),"\n";
+                }
+            }
+        }
 
-foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
-    print $school_name,",",$total_hash{$school_name},"\n";
-}
+        print FILE_OUT "---------------------------------------- Team Points & Starts\n";
+        print FILE_OUT "School,Points,Starts,Points/Start\n";
+        foreach $school (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
+            print FILE_OUT $school,",",$total_hash{$school},",",$total_starts_hash{$school},",",sprintf("%0.2f",$total_hash{$school}/$total_starts_hash{$school}),"\n";
+            print "    ",$school,",",$total_hash{$school},",",$total_starts_hash{$school},",",sprintf("%0.2f",$total_hash{$school}/$total_starts_hash{$school}),"\n";
+        }
 
 #foreach $school_name (sort keys %total_hash){
 #    print $school_name,",",$total_hash{$school_name},"\n";
 #}
 
-foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
-    print ",",$school_name;
-}
+        print FILE_OUT "---------------------------------------- Event Points\n";
+        foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
+            print FILE_OUT ",",$school_name;
+        }
 
-foreach $event_name (sort keys %event_hash){
-    print "\n",$event_name;
-    foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
-        $points = 0;
-        foreach $skater_info (@{$event_hash{$event_name}}){
-            if ($skater_info =~ m/$school_name/){
-                $points = $event_hash_total{$event_name}{$school_name};
+        foreach $event_name (sort keys %event_hash){
+            print FILE_OUT "\n",$event_name;
+            foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
+                $points = 0;
+                foreach $skater_info (@{$event_hash{$event_name}}){
+                    if ($skater_info =~ m/$school_name/){
+                        $points = $event_hash_total{$event_name}{$school_name};
+                    }
+                }
+                print FILE_OUT ",",$points;
             }
         }
-        print ",",$points;
+
+        print FILE_OUT "\n";
+        foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
+            print FILE_OUT ",",$school_name;
+        }
+
+        foreach $event_name (sort keys %event_hash){
+            print FILE_OUT "\n",$event_name;
+            foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
+#                print $school_name,": ",$starts_hash{$event_name}{$school_name},"\n";
+                if ($starts_hash{$event_name}{$school_name}!=0){
+                    print FILE_OUT ",",$starts_hash{$event_name}{$school_name};
+                }
+                else{
+                    print FILE_OUT ",0";
+                }
+            }
+        }
     }
+    else {
+        print "  Skipping   ",$_,"\n";
+    }
+    close FILE_RESULTS;
+    close FILE_OUT;
 }
 
-print "\n";
-foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
-    print ",",$school_name;
-}
 
-foreach $event_name (sort keys %event_hash){
-    print "\n",$event_name;
-    foreach $school_name (sort {$total_hash{$b} <=> $total_hash{$a}} keys %total_hash){
-        if ($event_hash_total{$event_name}{$school_name}){
-        print ",",$starts_hash{$event_name}{$school_name};
-        }
-        else{
-            print ",0";
-        }
-    }
-}
