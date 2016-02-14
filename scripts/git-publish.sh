64f3ebf818c20f8157d3e6b63ec5490eec4f2992
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2016-02-14 16:24:12 -0500

    Update git-publish

--- scripts/git-publish.sh
+++ scripts/git-publish.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash +x
 
 file=$0
 
 git checkout gh-pages
 cp $file .
 git status | grep $file 2>&1 > /dev/null
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
     echo "[INFO] Commiting and publishing to GitHub"
     # git add $file
     # git commit -m "Update index.html"
